07feb69b478e4d1180aedbc2c6fd8bb6f8bfcfd4
musasabi333 <musasabi333@gmail.com>
2016-12-07 22:18:46 +0100

    Adding Bullet

---
+++ Assets/BulletBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+
+    public int energyDestroy = 1;
+    public int speed = 1;
+    public Vector2 tankOrientation;
+	
+	void Update () {
+        transform.Translate(tankOrientation * speed);
+	}
+    
+}
