ca6e26cf21e94951b0300147b4457cf60bc88ac2
musasabi333 <musasabi333@gmail.com>
2016-12-07 21:01:07 +0100

    Added wall

---
+++ Assets/WallBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBehaviour : MonoBehaviour {
+
+    public int energy;
+    public Sprite brokenSprite;
+	// Use this for initialization
+	void Start () {
+        energy = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag.Equals("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            energy--;
+            if (energy == 0)
+                Destroy(this.gameObject);
+            else if (energy == 1)
+                this.GetComponent<SpriteRenderer>().sprite = brokenSprite;
+           
+        }
+    }
+}
